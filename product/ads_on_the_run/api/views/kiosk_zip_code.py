7b3226f1421978f09eed3fb031435e39f27549a1
pinhao1994 <pinhao1994@gmail.com>
2018-11-30 18:32:47 -0500

    import kiosk helper function

--- product/ads_on_the_run/api/views/kiosk_zip_code.py
+++ product/ads_on_the_run/api/views/kiosk_zip_code.py
@@ -1,5 +1,8 @@
 import pickle
 import json
+import pandas as pd
+from shapely.geometry import Point, LineString
+import polyline
 
 from django.http import HttpResponse
 
@@ -21,3 +24,23 @@ def index(request, zip_code):
 
     return HttpResponse(json.dumps({'vals': [['long1', 'lat1'],
                                              ['long2', 'lat2']]}))
+
+
+def kiosk_distance(kiosk):
+	strava = pd.read_csv('../data-collect/NYC_Strava.csv')
+	seg_list_poly = strava['map_polyline'].values.tolist()
+	seg_list_gpd = list(map(lambda x: LineString(polyline.decode(x)), seg_list_poly))
+
+	return min(list(map(lambda x: x.distance(kiosk), seg_list_gpd)))
+
+
+def rank_kiosk(zipcode):
+	kiosk = pd.read_csv('../data-collect/LinkNYC_Kiosk_Status.csv', index_col="SITE ID")
+
+	kiosk = kiosk.loc[kiosk['ZIP'] == zipcode]
+	kiosk['lat_lon'] = list(zip(kiosk['LATITUDE'], kiosk['LONGITUDE']))
+	kiosk['Point'] = list(map(Point, list(zip(kiosk['LONGITUDE'], kiosk['LATITUDE']))))
+	kiosk['Distance'] = kiosk.apply(lambda row: kiosk_distance(row['Point']), axis=1)
+	kiosk = kiosk.sort_values('Distance')
+	kiosk_lat_lon_list = kiosk['lat_lon'].tolist()
+	return kiosk_lat_lon_list
